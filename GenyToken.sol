29ccc2e2a8d1d2f284b6501c8b340f2ecb9186d2
C O M P Ξ Z <kambiz.ceo@gmail.com>
2025-04-29 12:08:47 +0330

    Update GenyToken.sol

--- GenyToken.sol
+++ GenyToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Genyleap
 
-pragma solidity 0.8.20;
+pragma solidity 0.8.29;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
